3afc5090bcba7dbacb37343980ecb69f1a80839e
coldkillerr <yonishitjain123456@gmail.com>
2020-08-20 21:39:22 +0530

    added main if conditional added a comment in main function

--- example.py
+++ example.py
@@ -1,4 +1,8 @@
 def git_opeation():
  print("I am adding example.py file to the remote repository.")
-git_opeation()
+
+if __name__ == '__main__':
+
+"""this code will run only when the file is run all by itself"""
+	git_opeation()
 
