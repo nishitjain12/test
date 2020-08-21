751a875993b4055ee39fe1e3cb060c930dde0cc6
coldkillerr <yonishitjain123456@gmail.com>
2020-08-21 13:45:28 +0530

    added a comment in main function

--- example.py
+++ example.py
@@ -1,6 +1,8 @@
 def git_opeation():
  print("I am adding example.py file to the remote repository.")
+
 if __name__ == '__main__':
 
+"""this code will run only when the file is run all by itself"""
 	git_opeation()
 
